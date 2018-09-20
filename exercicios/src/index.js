03f79c641c326105c4261818b48eb6a2b5b930da
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-09-20 09:01:18 -0300

    Componentes com parametros

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Primeiro from './componentes/Primeiro'
+//import Primeiro from './componentes/Primeiro'
+import Bomdia from './componentes/Bomdia'
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+ReactDOM.render(<Bomdia nome='Pedro' saudacao=' Boa noite'/>, document.getElementById('root'))
