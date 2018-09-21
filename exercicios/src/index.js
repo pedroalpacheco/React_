0c6e2a9dc9933085829d3e4f36f23635eab8ae86
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-09-21 09:21:01 -0300

    Multiplos componentes

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //import Primeiro from './componentes/Primeiro'
-import Bomdia from './componentes/Bomdia'
+//import Bomdia from './componentes/Bomdia'
+//import Timer from './componentes/Time'
+import {BomDia, BoaTarde} from './componentes/Multiplos'
 
-ReactDOM.render(<Bomdia nome='Pedro' saudacao=' Boa noite'/>, document.getElementById('root'))
+//ReactDOM.render(<Bomdia nome='Pedro' saudacao=' Boa noite'/>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+    <BomDia nome = "Fernanda"/>
+    <BoaTarde nome = "Fê"/>
+    </div>, 
+    document.getElementById('root'))
