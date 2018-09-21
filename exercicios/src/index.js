5357b5a736bcba419351da5f8970bd0bd2b418bc
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-09-21 14:18:32 -0300

    Componente Classe

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom'
 //import Primeiro from './componentes/Primeiro'
 //import Bomdia from './componentes/Bomdia'
 //import Timer from './componentes/Time'
-import {BomDia, BoaTarde} from './componentes/Multiplos'
+//import {BomDia, BoaTarde} from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
 //ReactDOM.render(<Bomdia nome='Pedro' saudacao=' Boa noite'/>, document.getElementById('root'))
-ReactDOM.render(
+/*ReactDOM.render(
     <div>
     <BomDia nome = "Fernanda"/>
     <BoaTarde nome = "Fê"/>
     </div>, 
-    document.getElementById('root'))
+    document.getElementById('root'))*/
+
+    ReactDOM.render(
+        <div>
+            <Saudacao tipo="Bom dia" nome="João"/>
+        </div>,
+        document.getElementById('root')
+    )
